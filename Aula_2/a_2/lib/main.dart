56acf73e4af0445093a8dd57b5eca56dec6b6eb8
danielcavali <56773714+danielcavali@users.noreply.github.com>
2021-06-27 21:56:02 -0300

    Inicio da composição de telas
    aprendemos como será composta nossas telas com Flutter.

---
+++ Aula_2/a_2/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(
+    Container(
+        child: Center(
+            child: Text('Aprendendo widgets',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(color: Colors.white, fontSize: 30.0)))),
+  );
+}
