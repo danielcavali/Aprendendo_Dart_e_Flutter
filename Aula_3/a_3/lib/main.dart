04f0a55c8fef8cb4de5454ddf6419bc8ebf8e349
danielcavali <56773714+danielcavali@users.noreply.github.com>
2021-06-29 09:51:57 -0300

    stateless
    aprendemos a diferença dos estados e usamos o stateless.

---
+++ Aula_3/a_3/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(AppWidget(
+    title: 'Aprendendo Estados',
+  ));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+            child: Text(title,
+                textDirection: TextDirection.ltr,
+                style: TextStyle(color: Colors.white, fontSize: 30.0))));
+  }
+}
