904f7de6aec2b61b09cc4002a432c96ac0857043
danielcavali <56773714+danielcavali@users.noreply.github.com>
2021-07-09 10:37:17 -0300

    rotas
    aprendemos como fazer a navegação entre telas manualmente e de maneira nomeada.

---
+++ Aula_9/a_9/lib/login_page.dart
@@ -0,0 +1,62 @@
+import 'package:a_9/home_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String email = "";
+  String password = "";
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                onChanged: (text) {
+                  email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    labelText: 'e-mail', border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                onChanged: (text) {
+                  password = text;
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                    labelText: 'senha', border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (email == 'teste@email.com' && password == '1234') {
+                    print('funcionou');
+                    Navigator.of(context).pushReplacementNamed('/home');
+                  } else {
+                    print('deu ruim aqui bicho');
+                  }
+                },
+                child: Text('Login'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
